7a5471fb9032d2ff1640bbaa8562b32f5084bf96
Hemant Bam <hemantbam9865@gmail.com>
2024-02-18 23:01:42 +0545

    service side setup

---
+++ server_Side/index.js
@@ -0,0 +1,44 @@
+// Import necessary modules
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { adminAuth, userAuth, getUsers } = require("./scheme/auth.js");
+const connectDB = require("./Database/database.js");
+const authroute = require("./Route/authroute.js");
+const path = require('path');
+
+
+// Create the Express app
+const app = express();
+const PORT = 5000;
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authroute);
+
+
+// Connecting to the database
+connectDB();
+
+// Start the server
+const server = app.listen(PORT, () =>
+  console.log(`Server connected to port ${PORT}`)
+);
+
+// Handling unhandled rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`An error occurred: ${err.message}`);
+  server.close(() => process.exit(1));
+});
+
+// Routes
+app.get("/adminauth", adminAuth, (req, res) => res.send("Admin Route"));
+app.get("/basicauth", userAuth, (req, res) => res.send("User Route"));
+app.get("/api/allusers", getUsers);
+
+// Serve static files
+app.use('/uploads/profile/', express.static(path.join(__dirname, 'uploads/profile')));
+
+
